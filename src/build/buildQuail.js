4d1a1c4c6a2bb7d6f33cba7477147cf2d0abb56e
Jesse Renee Beach <splendidnoise@gmail.com>
2015-11-27 18:43:24 -0800

    Further changes to buildQuail

--- src/build/buildQuail.js
+++ src/build/buildQuail.js
@@ -6,41 +6,45 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 const quailDevelopmentFilesPath = __dirname + '/../src/development/**/*.js';
 
-glob(quailDevelopmentFilesPath, function (error, developmentFiles) {
-  if (error) {
-    process.exit(1);
-  }
-  mkdirp('dist', function (err) {
-    if (err) {
-      console.log(err);
+const cwd = process.cwd();
+
+function browserify () {
+  glob(quailDevelopmentFilesPath, function (error, developmentFiles) {
+    if (error) {
+      process.exit(1);
     }
-    else {
-      browserify({
-        entries: developmentFiles,
-        paths: [
-          './config/',
-          './src/core/',
-          './src/core/lib',
-          './src/js/',
-          './src/js/components/',
-          './src/js/strings/',
-          './src/assessments/',
-          './vendor/',
-        ],
-        options: {
-          debug: false
-        }
-      })
-        .add('./config/AllTests.js')
-        .transform(babelify)
-        .bundle()
-        .on('error', function (err) {
-          console.log('Error : ' + err.message);
+    mkdirp('dist', function (err) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        browserify({
+          entries: developmentFiles,
+          paths: [
+            './config/',
+            './src/core/',
+            './src/core/lib',
+            './src/js/',
+            './src/js/components/',
+            './src/js/strings/',
+            './src/assessments/',
+            './vendor/',
+          ],
+          options: {
+            debug: false
+          }
         })
-        .pipe(fs.createWriteStream('dist/runInBrowser.js'));
-    }
+          .add('./config/AllTests.js')
+          .transform(babelify)
+          .bundle()
+          .on('error', function (err) {
+            console.log('Error : ' + err.message);
+          })
+          .pipe(fs.createWriteStream('dist/runInBrowser.js'));
+      }
+    });
   });
-});
+}
 
 module.exports = function buildQuail (config) {
 
