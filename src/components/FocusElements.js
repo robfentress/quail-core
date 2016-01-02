ac628d14777ccc2fc03776023fc0017ab6914282
Jesse Renee Beach <splendidnoise@gmail.com>
2016-01-01 17:42:52 -0800

    FocusElements now returns an array of elements

--- src/components/FocusElements.js
+++ src/components/FocusElements.js
@@ -1,18 +1,25 @@
 /**
  * Elements that can (naturally) receive keyboard focus.
  */
-const FocusElements = [
-  'a[href]',
-  'area[href]',
-  'input:not([disabled])',
-  'select:not([disabled])',
-  'textarea:not([disabled])',
-  'button:not([disabled])',
-  'iframe',
-  'object',
-  'embed',
-  '*[tabindex]',
-  '*[contenteditable]'
-].join(', ');
+const DOM = require('DOM');
+const FocusElements = function (context) {
+  let selector = [
+    'a[href]',
+    'area[href]',
+    'input',
+    'select',
+    'textarea',
+    'button',
+    'iframe',
+    'object',
+    'embed',
+    '*[tabindex]',
+    '*[contenteditable]'
+  ].join(', ');
+  let elements = DOM
+    .scry(selector, context)
+    .filter((element) => !element.disabled);
+  return elements;
+};
 
 module.exports = FocusElements;
